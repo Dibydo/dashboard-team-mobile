8c9158920cf2d5cf690bceef5e8bfdc728e779de
megamax625 <kucenkovm@yandex.ru>
2023-10-18 19:41:32 +0300

    Simulation test fixes and main page scuffold testing complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Radio Wave Intensity',
       home: MyHomePage()
     );
@@ -19,11 +23,14 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-List<Map<String, dynamic>> data = []; // List to store the decoded JSON data
+
 class _MyHomePageState extends State<MyHomePage> {
+  List<Map<String, dynamic>> globalData = []; // List to store the decoded JSON data
   final String url = 'http://monitor.yss.su:8000/json';
 
   @override
@@ -36,16 +43,16 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
+        title: const Text('Dashboard')
       ),
       body: ListView.builder(
-        itemCount: data.length,
+        itemCount: globalData.length,
         itemBuilder: (context, index) {
-          final data_temp = data[index];
-          final id = data_temp['id'];
-          final amplitude = data_temp['amplitude'];
-          final frequency = data_temp['frequency'];
-          final datetime = data_temp['date'] + data_temp['time'];
+          final data = globalData[index];
+          final id = data['id'];
+          final amplitude = data['amplitude'];
+          final frequency = data['frequency'];
+          final datetime = data['date'] + data['time'];
 
           return ListTile(
             title: Text('ID: $id'),
@@ -55,31 +62,135 @@ class _MyHomePageState extends State<MyHomePage> {
                 Text('Amplitude: $amplitude'),
                 Text('Frequency: $frequency'),
                 Text('Datetime: $datetime'),
-                // Assuming 'time' is defined somewhere
-              ],
+              ]
             ),
             onTap: () {
               Navigator.of(context).push(
                 MaterialPageRoute(
-                  builder: (context) =>
-                      RequestScreen(
-                        url: url,
-                        amplitude: amplitude,
-                        id: id,
-                        frequency: frequency,
-                      ),
-                ),
+                  builder: (context) => RequestScreen(
+                    url: url,
+                    amplitude: amplitude,
+                    id: id,
+                    frequency: frequency)
+                    )
+                  );
+                }
               );
-            },
-          );
-        },
-      ),
-    );
-  }
+            }
+          )
+        );
+      }
 
+
+  // Future<void> fetchData() async {
+  //   var jsonData = "";
+  //   try {
+  //     final response = await http.get(Uri.parse(url));
+  //     if (response.statusCode == 200) {
+  //       final jsonData = json.decode(response.body);
+  //       if (kDebugMode) {
+  //         print(jsonData);
+  //       }
+  //     } else {
+  //       if (kDebugMode) {
+  //         print('Failed to load data. Status code: ${response.statusCode}');
+  //       }
+  //     }
+  //   } catch (e) {
+  //     if (kDebugMode) {
+  //       print('Error: $e');
+  //     }
+  //   }
 
   void fetchData() {
-    String jsondata = '''
+    // Simulate fetching JSON data
+    String jsonMain = '''
+  {
+    "data": [
+      {
+        "id": 1,
+        "amplitude": 10,
+        "frequency": 100,
+        "time": "09:30:45",
+        "date": "2023-09-21"
+      },
+      {
+        "id": 2,
+        "amplitude": 15,
+        "frequency": 120,
+        "time": "10:15:22",
+        "date": "2023-09-21"
+      },
+      {
+        "id": 3,
+        "amplitude": 8,
+        "frequency": 80,
+        "time": "14:45:10",
+        "date": "2023-09-22"
+      }
+    ]
+  }
+  ''';
+
+    Map<String, dynamic> parsedJson = json.decode(jsonMain);
+    List<Map<String, dynamic>> data = List<Map<String, dynamic>>.from(parsedJson['data']);
+
+    setState(() {
+      globalData = data;
+    });
+  }
+}
+
+class RequestScreen extends StatelessWidget {
+  final String url;
+  final double amplitude;
+  final int id;
+  final double frequency;
+
+  const RequestScreen({super.key,
+    required this.url,
+    required this.amplitude,
+    required this.id,
+    required this.frequency
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final requestText = 'Requesting URL http://$url/$amplitude/$id/$frequency';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Request Details'),
+      ),
+      body: Center(
+        child: Text(requestText),
+      )
+    );
+  }
+}
+
+class _MyGraphPage extends State<MyHomePage> {
+  List<Map<String, dynamic>> data = []; // List to store the decoded JSON data
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() {
+    // Simulate fetching JSON data
+    String jsonData = '''
+      "data": [
+        {"time": "2023-09-21 08:00:00", "intensity": 75.5},
+        {"time": "2023-09-21 08:15:00", "intensity": 80.2},
+        {"time": "2023-09-21 08:30:00", "intensity": 85.1},
+        {"time": "2023-09-21 08:45:00", "intensity": 79.8},
+        {"time": "2023-09-21 09:00:00", "intensity": 82.3},
+        {"time": "2023-09-21 09:15:00", "intensity": 77.6}
+      ]
+    ''';
+    String jsonMain = '''
       {
     "data": [
       {
@@ -106,112 +217,28 @@ class _MyHomePageState extends State<MyHomePage> {
     ]
   }
     ''';
+    String jsonAmplitude = '''
+      "data": [
+        {"time": "2023-09-21 08:00:00", "amplitude": 75.5},
+        {"time": "2023-09-21 08:15:00", "amplitude": 80.2},
+        {"time": "2023-09-21 08:30:00", "amplitude": 85.1},
+        {"time": "2023-09-21 08:45:00", "amplitude": 79.8},
+        {"time": "2023-09-21 09:00:00", "amplitude": 82.3},
+        {"time": "2023-09-21 09:15:00", "amplitude": 77.6}
+      ]
+    ''';
     List<Map<String, dynamic>> decodedData =
-        List<Map<String, dynamic>>.from(json.decode(jsondata));
+        List<Map<String, dynamic>>.from(json.decode(jsonData));
     setState(() {
       data = decodedData;
     });
   }
-}
-
-class RequestScreen extends StatelessWidget {
-  final String url;
-  final double amplitude;
-  final int id;
-  final double frequency;
-
-  RequestScreen({
-    required this.url,
-    required this.amplitude,
-    required this.id,
-    required this.frequency
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final requestText = 'Requesting URL http://$url/$amplitude/$id/$frequency';
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Request Details'),
-      ),
-      body: Center(
-        child: Text(requestText),
-      )
-    );
-  }
-  }
-
-// class _MyGraphPage extends State<MyGraphPage> {
-//   List<Map<String, dynamic>> data = []; // List to store the decoded JSON data
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     fetchData();
-//   }
-//
-//   void fetchData() {
-//     // Simulate fetching JSON data
-//     String jsonData = '''
-//       "data": [
-//         {"time": "2023-09-21 08:00:00", "intensity": 75.5},
-//         {"time": "2023-09-21 08:15:00", "intensity": 80.2},
-//         {"time": "2023-09-21 08:30:00", "intensity": 85.1},
-//         {"time": "2023-09-21 08:45:00", "intensity": 79.8},
-//         {"time": "2023-09-21 09:00:00", "intensity": 82.3},
-//         {"time": "2023-09-21 09:15:00", "intensity": 77.6}
-//       ]
-//     ''';
-//     String jsonMain = '''
-//       {
-//     "data": [
-//       {
-//         "id": 1,
-//         "amplitude": 10,
-//         "frequency": 100,
-//         "time": "09:30:45",
-//         "date": "2023-09-21"
-//       },
-//       {
-//         "id": 2,
-//         "amplitude": 15,
-//         "frequency": 120,
-//         "time": "10:15:22",
-//         "date": "2023-09-21"
-//       },
-//       {
-//         "id": 3,
-//         "amplitude": 8,
-//         "frequency": 80,
-//         "time": "14:45:10",
-//         "date": "2023-09-22"
-//       }
-//     ]
-//   }
-//     ''';
-//     String jsonAmplitude = '''
-//       "data": [
-//         {"time": "2023-09-21 08:00:00", "amplitude": 75.5},
-//         {"time": "2023-09-21 08:15:00", "amplitude": 80.2},
-//         {"time": "2023-09-21 08:30:00", "amplitude": 85.1},
-//         {"time": "2023-09-21 08:45:00", "amplitude": 79.8},
-//         {"time": "2023-09-21 09:00:00", "amplitude": 82.3},
-//         {"time": "2023-09-21 09:15:00", "amplitude": 77.6}
-//       ]
-//     ''';
-//     List<Map<String, dynamic>> decodedData =
-//         List<Map<String, dynamic>>.from(json.decode(jsonData));
-//     setState(() {
-//       data = decodedData;
-//     });
-//   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Radio Wave Intensity'),
+        title: const Text('Radio Wave Intensity'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -273,4 +300,4 @@ class RequestScreen extends StatelessWidget {
       ),
     );
   }
-
+}
