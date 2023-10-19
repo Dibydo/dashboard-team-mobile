0b20e9f0e203b71a38ffbf7845b88810e48592e2
Sarvarova Maria <82003911+cupotato@users.noreply.github.com>
2023-10-19 12:55:47 +0300

    Update main.dart
    графики

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'dart:convert';
@@ -18,8 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Radio Wave Intensity',
-      home: MyHomePage()
+        title: 'Radio Wave Intensity',
+        home: MyHomePage()
     );
   }
 }
@@ -51,8 +51,8 @@ class _MyHomePageState extends State<MyHomePage> {
         Map<String, dynamic> parsedJson = json.decode(response.body);
         List<Map<String, dynamic>> data = List<Map<String, dynamic>>.from(parsedJson['data']);
         setState(() {
-              globalData = data;
-            });
+          globalData = data;
+        });
         print(globalData);
       } else {
         print('Failed to fetch data: ${response.statusCode}');
@@ -65,105 +65,180 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard')
-      ),
-      body: ListView.builder(
-        itemCount: globalData.length,
-        itemBuilder: (context, index) {
-          final data = globalData[index];
-          final id = data['id'];
-          final amplitude = data['power'];
-          final frequency = data['freq'];
-          final datetime = data['date'] + " " + data['time'];
+        appBar: AppBar(
+            title: const Text('Dashboard')
+        ),
+        body: ListView.builder(
+            itemCount: globalData.length,
+            itemBuilder: (context, index) {
+              final data = globalData[index];
+              final id = data['id'];
+              final amplitude = data['power'];
+              final frequency = data['freq'];
+              final datetime = data['date'] + " " + data['time'];
 
-          return ListTile(
-            title: Text('ID: $id'),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Amplitude: $amplitude'),
-                Text('Frequency: $frequency'),
-                Text('Datetime: $datetime'),
-              ]
-            ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => RequestScreen(
-                    url: url,
-                    amplitude: amplitude,
-                    id: id,
-                    frequency: frequency)
-                    )
-                  );
-                }
+              return ListTile(
+                  title: Text('ID: $id'),
+                  subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Amplitude: $amplitude'),
+                        Text('Frequency: $frequency'),
+                        Text('Datetime: $datetime'),
+                      ]
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => _MyGraphPage(),
+                        ),
+                    );
+                  }
               );
             }
-          )
-        );
-      }
+        )
+    );
+  }
 
 
-  // Future<void> fetchData() async {
-  //   var jsonData = "";
-  //   try {
-  //     final response = await http.get(Uri.parse(url));
-  //     if (response.statusCode == 200) {
-  //       final jsonData = json.decode(response.body);
-  //       if (kDebugMode) {
-  //         print(jsonData);
-  //       }
-  //     } else {
-  //       if (kDebugMode) {
-  //         print('Failed to load data. Status code: ${response.statusCode}');
-  //       }
-  //     }
-  //   } catch (e) {
-  //     if (kDebugMode) {
-  //       print('Error: $e');
-  //     }
-  //   }
+// Future<void> fetchData() async {
+//   var jsonData = "";
+//   try {
+//     final response = await http.get(Uri.parse(url));
+//     if (response.statusCode == 200) {
+//       final jsonData = json.decode(response.body);
+//       if (kDebugMode) {
+//         print(jsonData);
+//       }
+//     } else {
+//       if (kDebugMode) {
+//         print('Failed to load data. Status code: ${response.statusCode}');
+//       }
+//     }
+//   } catch (e) {
+//     if (kDebugMode) {
+//       print('Error: $e');
+//     }
+//   }
 
-  // void fetchData() {
-  //   // Simulate fetching JSON data
-  //   String jsonMain = '''
-  // {
-  //   "data": [
-  //     {
-  //       "id": 1,
-  //       "amplitude": 10,
-  //       "frequency": 100,
-  //       "time": "09:30:45",
-  //       "date": "2023-09-21"
-  //     },
-  //     {
-  //       "id": 2,
-  //       "amplitude": 15,
-  //       "frequency": 120,
-  //       "time": "10:15:22",
-  //       "date": "2023-09-21"
-  //     },
-  //     {
-  //       "id": 3,
-  //       "amplitude": 8,
-  //       "frequency": 80,
-  //       "time": "14:45:10",
-  //       "date": "2023-09-22"
-  //     }
-  //   ]
-  // }
-  // ''';
-  //
-  //   Map<String, dynamic> parsedJson = json.decode(jsonMain);
-  //   List<Map<String, dynamic>> data = List<Map<String, dynamic>>.from(parsedJson['data']);
-  //
-  //   setState(() {
-  //     globalData = data;
-  //   });
-  // }
+// void fetchData() {
+//   // Simulate fetching JSON data
+//   String jsonMain = '''
+// {
+//   "data": [
+//     {
+//       "id": 1,
+//       "amplitude": 10,
+//       "frequency": 100,
+//       "time": "09:30:45",
+//       "date": "2023-09-21"
+//     },
+//     {
+//       "id": 2,
+//       "amplitude": 15,
+//       "frequency": 120,
+//       "time": "10:15:22",
+//       "date": "2023-09-21"
+//     },
+//     {
+//       "id": 3,
+//       "amplitude": 8,
+//       "frequency": 80,
+//       "time": "14:45:10",
+//       "date": "2023-09-22"
+//     }
+//   ]
+// }
+// ''';
+//
+//   Map<String, dynamic> parsedJson = json.decode(jsonMain);
+//   List<Map<String, dynamic>> data = List<Map<String, dynamic>>.from(parsedJson['data']);
+//
+//   setState(() {
+//     globalData = data;
+//   });
+// }
 }
+class _MyGraphPage extends StatelessWidget {
+  final Random random = Random();
 
+  List<Map<String, dynamic>> generateRandomData() {
+    List<Map<String, dynamic>> data = [];
+    for (int i = 0; i < 6; i++) {
+      data.add({
+        'time': '2023-09-21 0$i:00:00',
+        'intensity': (random.nextInt(201) - 100).toDouble(),
+      });
+    }
+    return data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, dynamic>> data = generateRandomData();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Radio Wave Intensity'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: LineChart(
+          LineChartData(
+            gridData: FlGridData(show: false),
+            titlesData: FlTitlesData(
+              leftTitles: SideTitles(
+                showTitles: true,
+                reservedSize: 30, // Adjust this value for label spacing
+                interval: 20, // Adjust this value based on your intensity range
+                getTitles: (value) {
+                  return value.toInt().toString(); // Customize label format as needed
+                },
+              ),
+              bottomTitles: SideTitles(
+                showTitles: true,
+                reservedSize: 30, // Adjust this value for label spacing
+                interval: 1, // Adjust this value based on your data frequency
+                getTitles: (value) {
+                  // Convert value (index) to corresponding time from data
+                  if (value >= 0 && value < data.length) {
+                    final time = data[value.toInt()]["time"] as String;
+                    return time.substring(11, 16); // Display time in HH:mm format
+                  }
+                  return '';
+                },
+              ),
+              topTitles: SideTitles(showTitles: false), // Hide top titles
+              rightTitles: SideTitles(showTitles: false), // Hide right titles
+            ),
+            borderData: FlBorderData(
+              show: true,
+              border: Border.all(color: const Color(0xff37434d), width: 1),
+            ),
+            minX: 0,
+            maxX: data.length.toDouble() - 1,
+            minY: -100,
+            maxY: 100,
+            // Adjust this based on your intensity data range
+            lineBarsData: [
+              LineChartBarData(
+                spots: data.asMap().entries.map((entry) {
+                  final index = entry.key;
+                  final intensity = entry.value['intensity'] as double;
+                  return FlSpot(index.toDouble(), intensity);
+                }).toList(),
+                isCurved: true,
+                colors: [Colors.blue],
+                dotData: FlDotData(show: false),
+                belowBarData: BarAreaData(show: false),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 class RequestScreen extends StatelessWidget {
   final String url;
   final double amplitude;
@@ -182,17 +257,17 @@ class RequestScreen extends StatelessWidget {
     final requestText = 'Requesting URL http://$url/$amplitude/$id/$frequency';
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Request Details'),
-      ),
-      body: Center(
-        child: Text(requestText),
-      )
+        appBar: AppBar(
+          title: const Text('Request Details'),
+        ),
+        body: Center(
+          child: Text(requestText),
+        )
     );
   }
 }
 
-class _MyGraphPage extends State<MyHomePage> {
+/*class _MyGraphPage extends State<MyHomePage> {
   List<Map<String, dynamic>> data = []; // List to store the decoded JSON data
 
   @override
@@ -325,3 +400,4 @@ class _MyGraphPage extends State<MyHomePage> {
     );
   }
 }
+*/
